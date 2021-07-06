875c14e590f85fc330d2a87e1aa9a2114e3f3013
Jamil Corwin <kajdhfglksdjhfglkhj@gmail.com>
2021-07-06 19:52:32 +0300

    Completed

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 
 exports.min = function min (array) {
-  if (!array?.length) return 0;
+  if (!array || !array.length) return 0;
   return array.reduce((acc, val) => acc < val ? acc : val, Number.MAX_SAFE_INTEGER);
 }
 
 exports.max = function max (array) {
-  if (!array?.length) return 0;
+  if (!array || !array.length) return 0;
   return array.reduce((acc, val) => acc > val ? acc : val, Number.MIN_SAFE_INTEGER);
 }
 
 exports.avg = function avg (array) {
-  if (!array?.length) return 0;
+  if (!array || !array.length) return 0;
   return array.reduce((acc, val) => acc + val, 0) / array.length;
 }
